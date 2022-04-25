34b904c4159a13a978fea100ef8458c35ecdecba
jmac222 <john.mcfarland222@gmail.com>
2022-04-24 22:44:31 -0700

    new fonts and more seasons

--- src/stats.js
+++ src/stats.js
@@ -6,6 +6,11 @@ const year2020 = document.querySelector(".year2020");
 const year2019 = document.querySelector(".year2019");
 const year2018 = document.querySelector(".year2018");
 const year2017 = document.querySelector(".year2017");
+const year2016 = document.querySelector(".year2016");
+const year2015 = document.querySelector(".year2015");
+const year2014 = document.querySelector(".year2014");
+const year2013 = document.querySelector(".year2013");
+const year2012 = document.querySelector(".year2012");
 const nameHeader = document.querySelector(".nameHeader");
 let year = "2021";
 
@@ -28,6 +33,26 @@ year2018.onclick = () => {
 };
 year2017.onclick = () => {
   year = "2017";
+  fetchStats();
+};
+year2016.onclick = () => {
+  year = "2016";
+  fetchStats();
+};
+year2015.onclick = () => {
+  year = "2015";
+  fetchStats();
+};
+year2014.onclick = () => {
+  year = "2014";
+  fetchStats();
+};
+year2013.onclick = () => {
+  year = "2013";
+  fetchStats();
+};
+year2012.onclick = () => {
+  year = "2012";
   fetchStats();
 };
 
